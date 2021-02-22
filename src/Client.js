8860b8953ba34f531e27c7c8fc269a5f76dfcf7d
Mercy Kipyegon <mercyjemosop@gmail.com>
2021-02-22 09:13:38 +0300

    .gitignore is now working

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
+  
   uri: 'https://api.github.com/graphql',
   headers: {
     Authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`
